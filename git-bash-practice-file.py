ba56a5f4e718d504a1cf3c2cf702728a5f233dbb
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2024-05-24 13:11:24 +0100

    Modified the code

--- git-bash-practice-file.py
+++ git-bash-practice-file.py
@@ -1,1 +1,15 @@
 print("Hello world")
+
+
+def add_something(sth=""):
+    if sth:
+        print("Something has been added: {0}".format(sth))
+    else:
+        print("nothing added")
+    pass
+
+
+name = "book list"
+
+add_something(name)
+add_something()
